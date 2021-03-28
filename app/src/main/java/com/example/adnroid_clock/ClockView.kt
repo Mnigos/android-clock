9fcf6135912b1d7671e19d570095cbf76602e3b2
MoneyIgos <makowskiigor@gmail.com>
2021-03-28 14:45:30 +0200

    feat: create ClockView class

---
+++ app/src/main/java/com/example/adnroid_clock/ClockView.kt
@@ -0,0 +1,7 @@
+package com.example.adnroid_clock
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClockView(c: Context, attrs: AttributeSet?, defStyleAttr: Int) : View(c, attrs, defStyleAttr)
