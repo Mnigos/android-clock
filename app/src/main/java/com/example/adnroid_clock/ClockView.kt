969da26405c71336b6862c5aed02ea2c503b0976
MoneyIgos <makowskiigor@gmail.com>
2021-03-28 14:56:23 +0200

    feat(ClockView): add onDraw method

--- app/src/main/java/com/example/adnroid_clock/ClockView.kt
+++ app/src/main/java/com/example/adnroid_clock/ClockView.kt
@@ -1,10 +1,12 @@
 package com.example.adnroid_clock
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import java.util.*
 
 class ClockView(c: Context, attrs: AttributeSet?, defStyleAttr: Int) : View(c, attrs, defStyleAttr) {
     private val paint = Paint()
@@ -14,5 +16,22 @@ class ClockView(c: Context, attrs: AttributeSet?, defStyleAttr: Int) : View(c, a
         paint.style = Paint.Style.FILL
         paint.textSize = 60F
         paint.isAntiAlias = true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        val calendar = Calendar.getInstance()
+        val hour: String = calendar.get(Calendar.HOUR_OF_DAY).toString()
+        val minute: String = calendar.get(Calendar.MINUTE).toString()
+        val second: String = calendar.get(Calendar.SECOND).toString()
+
+        canvas?.drawColor(Color.WHITE)
+        canvas?.drawText(hour, width / 2F - 200F, height / 2F, paint)
+        canvas?.drawText(minute, width / 2F - 100F, height / 2F, paint)
+        canvas?.drawText(second, width / 2F, height / 2F, paint)
+
+        postInvalidateDelayed(500)
+        invalidate()
+
+        super.onDraw(canvas)
     }
 }
