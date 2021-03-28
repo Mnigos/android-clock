a216904f1dbc7880d1e8e6c5f41c63368030b4a2
MoneyIgos <makowskiigor@gmail.com>
2021-03-28 14:49:13 +0200

    feat(ClockView): create init

--- app/src/main/java/com/example/adnroid_clock/ClockView.kt
+++ app/src/main/java/com/example/adnroid_clock/ClockView.kt
@@ -1,7 +1,18 @@
 package com.example.adnroid_clock
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class ClockView(c: Context, attrs: AttributeSet?, defStyleAttr: Int) : View(c, attrs, defStyleAttr)
+class ClockView(c: Context, attrs: AttributeSet?, defStyleAttr: Int) : View(c, attrs, defStyleAttr) {
+    private val paint = Paint()
+
+    init {
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.FILL
+        paint.textSize = 60F
+        paint.isAntiAlias = true
+    }
+}
