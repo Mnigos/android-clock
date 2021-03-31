610e27eabbf03c4c078cd711c48a574c7d2332d7
MoneyIgos <makowskiigor@gmail.com>
2021-03-31 10:29:22 +0200

    feat(MainActivity.kt): add getFirstDigit fun to MainActivity

--- app/src/main/java/com/example/adnroid_clock/MainActivity.kt
+++ app/src/main/java/com/example/adnroid_clock/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private fun getFirstDigit(num: Int): Int {
+        return if (num > 9) num.toString().substring(0..1).toInt()
+        else 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
